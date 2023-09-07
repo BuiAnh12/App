31ce5ae7c72e232ffb9f2b755f9a45a83db42a90
Tom <buianh120403@gmail.com>
2023-09-07 16:34:08 +0700

    Update model

--- predict.py
+++ predict.py
@@ -1,71 +1,56 @@
 import streamlit as st
 import numpy as np
-from PIL import Image, ImageOps  # Import ImageOps
+from PIL import Image, ImageOps
 import joblib
 from streamlit_drawable_canvas import st_canvas
-from sklearn.preprocessing import StandardScaler
-
-
-@st.cache(allow_output_mutation=True)
-def preprocess_image(image):
-    # Resize the image to 28x28 pixels
-    image = image.resize((28, 28))
-
-    # Convert to grayscale
-    image = image.convert("L")
-
-    # Invert colors (black background, white drawing)
-    image = ImageOps.invert(image)
-
-    # Convert to a NumPy array
-    image_array = np.array(image)
-
-    # Normalize the image manually (0 to 1)
-    normalized_image = image_array.astype(np.float32) / 255.0
-
-    # Flatten the 2D array to a 1D array
-    flattened_image = normalized_image.flatten()
-
-    # Reshape the flattened image into a 2D array with a single sample
-    flattened_image = flattened_image.reshape(1, -1)
-
-    # Return the reshaped and scaled image
-    return flattened_image
+import cv2
+import pickle
 
 
 # Load your SVM model with st.cache
 @st.cache(allow_output_mutation=True)
 def load_model():
-    return joblib.load("svm_model_rbf_C10.joblib")
+    model_file = open("mnist_rbf_defaut.pickle", "rb")
+    return pickle.load(model_file)
+    # return joblib.load('../svm_model_rbf_C10.joblib')
 
 
+# Load the SVM model
 model = load_model()
 
 st.title("Figure Detection App")
 
-# Create a canvas for drawing
 canvas = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=3,
-    stroke_color="white",
-    background_color="black",
+    stroke_width=70,
+    stroke_color="white",  # Đặt màu nét vẽ thành màu trắng (255, 255, 255 là màu trắng)
+    background_color="black",  # Đặt màu nền thành màu đen (0, 0, 0 là màu đen)
     drawing_mode="freedraw",
     key="drawing_canvas",
-    height=300,
-    width=400,
+    height=500,
+    width=500,
 )
-
 if st.button("Detect Figure"):
     if canvas.image_data is not None:
-        # Convert the canvas drawing to an image
-        img_data = canvas.image_data.astype("uint8")
-        im = Image.fromarray(img_data, mode="RGBA")
+        try:
+            # Convert the canvas drawing to a NumPy array
+            img_data = np.array(canvas.image_data, dtype=np.uint8)
+            # Resize the image to 28x28 pixels
+            img_data = img_data[:, :, :3]
+            img_data = cv2.resize(img_data, (28, 28))
+            # Convert to grayscale
+            img_data_gray = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
+            flattened_image = img_data_gray.flatten()
+            # Reshape the flattened image into a 2D array with a single sample
+            flattened_image = flattened_image.reshape(1, -1)
+            # Use the SVM model to make predictions
+            prediction = model.predict(flattened_image)
 
-        # Preprocess the image
-        processed_image = preprocess_image(im)
+            # Display the processed image
+            st.image(img_data_gray, caption="Processed Image")
 
-        # Use the SVM model to make predictions
-        prediction = model.predict(processed_image)
-
-        # Display the prediction result
-        st.write(f"Predicted Figure: {prediction[0]}")
+            # Display the prediction result
+            st.write(f"Predicted Figure: {prediction[0]}")
+        except Exception as e:
+            st.write(f"Error processing image: {str(e)}")
+    else:
+        st.write("Vui lòng vẽ trước khi nhấn 'Detect Figure'.")
